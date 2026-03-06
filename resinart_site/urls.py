483c2bd2662a95629da3a84d228f63d8e32ae2cd
tishutaneja-gif <tishutaneja@gmail.com>
2026-03-06 15:34:12 +0530

    Update urls.py

--- resinart_site/urls.py
+++ resinart_site/urls.py
@@ -5,8 +5,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("setup-db/", views.setup_database),
     path('', include('store.urls')),
 ]
 
 if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
